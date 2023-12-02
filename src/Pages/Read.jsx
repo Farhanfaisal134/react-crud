a1e4f98b70a829971bafd5106d3365bf7a6aabc5
o3190474834@gmail.com <133948706+Farhanfaisal134@users.noreply.github.com>
2023-12-02 18:25:39 +0500

    search funcnilty

--- src/Pages/Read.jsx
+++ src/Pages/Read.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Read = () => {
   const [data, setData] = useState([]);
+  const [input, setInput] = useState('')
 
   function getData() {
     axios
@@ -25,34 +26,49 @@ const Read = () => {
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
   }
+  const inputHandler = (e) => {
+    setInput(e.target.value.toLowerCase())
+  }
   useEffect(() => {
     getData();
   }, []);
   return (
-    <div className='max-w-5xl mx-auto'>
-      <h1 className='text-4xl font-medium text-center mb-3'>Read Data</h1>
-      <div className='flex flex-col w-full gap-4'>
-        {
-          data.map((item) => (
-            <div className='grid grid-cols-5 gap-3 items-center px-4 py-3 border border-b-black w-full'>
-              <span>{item.id}</span>
-              <h1>{item.name}</h1>
-              <h2 className='col-span-2'>{item.email}</h2>
-              <div className='flex gap-1 items-center w-full'>
-                <Link to="/update">  <button className='bg-green-300 px-3 py-2 rounded-sm text-gray-400'
-                  onClick={() =>
-                    setToLocalStorage(
-                      item.id,
-                      item.name,
-                      item.email
-                    )}>
-                  Edit</button></Link>
-                <button className='bg-red-400 px-3 py-2 rounded-sm text-white-400' onClick={() => handleDelete(item.id)}>Del</button>
-                <Link to='/'>
-                  <button type="submit" className="bg-blue-500 text-white text-xl border-none outline-none px-3 py-2 rounded-md shadow-md mx-auto">Create</button></Link>
-              </div>
+    <div className='max-w-5xl mx-auto '>
+      <h1 className='text-4xl font-medium text-center mb-4'>Read Data</h1>
+      <div className='flex flex-col justify-center items-center gap-2 my-2 '>
+        <label className='text-3xl font-medium'>Seach_Item</label>
+        <input className='border border-gray-500 px-3 py-2 rounded-sm outline-none text-xl w-[50%]'
+          onChange={inputHandler} />
+      </div>
+      <div className='flex flex-col w-full gap-4 py-4'>
+        {data.filter((ele) => {
+          if (ele === '') {
+            return ele
+          } else {
+            return (ele.name.toLowerCase().includes(input) ||
+              (ele.email.toLowerCase().includes(input))
+            )
+          }
+        }).map((item) => (
+          <div className='grid md:grid-cols-5 gap-3 items-center md:text-left justify-center text-center px-4 py-3 border border-b-black w-full'>
+            <span className='text-2xl capitalize font-medium'>{item.id}</span>
+            <h1 className='text-2xl capitalize'>{item.name}</h1>
+            <h2 className=' md:col-span-2 text-2xl'>{item.email}</h2>
+            <div className='flex gap-1 items-center w-full'>
+              <Link to="/update">  <button className='bg-green-300 px-3 py-2 rounded-sm text-gray-400'
+                onClick={() =>
+                  setToLocalStorage(
+                    item.id,
+                    item.name,
+                    item.email
+                  )}>
+                Edit</button></Link>
+              <button className='bg-red-400 px-3 py-2 rounded-sm text-white-400' onClick={() => handleDelete(item.id)}>Del</button>
+              <Link to='/'>
+                <button type="submit" className="bg-blue-500 text-white text-xl border-none outline-none px-3 py-2 rounded-md shadow-md mx-auto">Create</button></Link>
             </div>
-          ))
+          </div>
+        ))
         }
       </div>
     </div >
